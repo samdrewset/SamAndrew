9d41ff992cc2448f9e1b449c68323b7e255ebfc7
samdrewset <samdrewset@gmail.com>
2020-02-13 15:55:15 +0700

    newPractice

--- inputPractice/calculator.js
+++ inputPractice/calculator.js
@@ -1,18 +1,18 @@
-function ngitung(){
-    var a = parseFloat(document.querySelector("#num1").value);
-    var b = parseFloat(document.querySelector("#num2").value);
-    var op = document.querySelector("#operator").value;
-    var calculate;
-
-    if (op == "add") {
-        calculate = a+b;
-    } else if (op == "min"){
-        calculate = a-b;
-    } else if (op == "multiply"){
-        calculate = a*b;
-    } else if (op == "divide"){
-        calculate = a/b;
+    function ngitung(){
+        var a = parseFloat(document.querySelector("#num1").value);
+        var b = parseFloat(document.querySelector("#num2").value);
+        var op = document.querySelector("#operator").value;
+        var calculate;
+        
+        if (op == "add") {
+            calculate = a+b;
+        } else if (op == "min"){
+            calculate = a-b;
+        } else if (op == "multiply"){
+            calculate = a*b;
+        } else if (op == "divide"){
+            calculate = a/b;
+        }
+        
+        document.querySelector("#result").innerHTML = calculate;
     }
-
-    document.querySelector("#result").innerHTML = calculate;
-}
